01b6964b2e4a9222e2d3d4dab05a02a03f92c253
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-02-22 15:02:47 -0300

    criando um contador

--- moveit/src/components/Button.tsx
+++ moveit/src/components/Button.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
+
 interface ButtonProps {
     color: string;
     children: string;
 }
 
 export function Button(props: ButtonProps) {
+    const [counter, setCounter] = useState(1)
+
+    function increment() {
+        setCounter(counter + 1);
+    }
     return(
-        <button type="button" style={{backgroundColor: props.color}}>
+        <button type="button"
+         style={{backgroundColor: props.color}}
+         onClick={increment}
+         >
             {/* {props.color} */}
-            {props.children}
+            {props.children} 
+            <strong>{counter}</strong>
         </button>
     );
 }
