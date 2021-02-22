af6bf06bc7669a642c6230d5ed290c1705898022
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-02-22 19:57:24 -0300

    finalizando dia um #rumoaoproximonivel

--- moveit/src/components/ExperienceBar.tsx
+++ moveit/src/components/ExperienceBar.tsx
@@ -4,6 +4,7 @@ export function ExperienceBar() {
             <span>0 xp</span>
             <div>
                 <div style={{ width: '50%'}}/>
+                <span className="current-experience" style={{ left: '50%'}}>300 xp</span>
             </div>
             <span>600 xp</span>
         </header>
